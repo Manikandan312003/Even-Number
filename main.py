b615792cc1dfd8034724f3a68e30dcea69303d3b
lab <manirajendran31jul@gmail.com>
2023-07-14 09:35:34 +0100

    first commit

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
-a=list(map(int,input().split()))
+a=list(map(int , input().split()))
 EvenList=[]
 count=0
 for i in a:
     if i%2==0:
         count+=1
     EvenList.append(count)
-# Query=list(map(int,input().split()))
 while(1):
-    a,b=map(int,input().split())
+    Input=input()
+    if Input =="":
+        break
+    a,b=map(int,Input.split())
     if a==0:
         print(EvenList[b])
         continue
---
+++ typecasting.py
@@ -0,0 +1,8 @@
+a:str=""
+a=4
+print(a)
+def typecast(a:str):
+    print(type(a))
+    pass
+
+typecast([123])
