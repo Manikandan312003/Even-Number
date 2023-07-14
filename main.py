c1e6f28d2a6905e18a57857ae8d5301f21e32f20
Manikandan312003 <106978783+Manikandan312003@users.noreply.github.com>
2023-07-14 09:41:03 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-a=list(map(int , input().split()))
-EvenList=[]
-count=0
+a = list(map(int, input().split()))
+EvenList = []
+count = 0
 for i in a:
-    if i%2==0:
-        count+=1
+    if i % 2 == 0:
+        count += 1
     EvenList.append(count)
-while(1):
-    Input=input()
-    if Input =="":
-        break
-    a,b=map(int,Input.split())
-    if a==0:
-        print(EvenList[b])
+while 1:
+    Input = input()
+    if Input != "":
+        a, b = map(int, Input.split())
+        if a == 0:
+            print(EvenList[b])
+            continue
+        print(EvenList[b] - EvenList[a - 1])
         continue
-    print(EvenList[b]-EvenList[a-1])
+    break
